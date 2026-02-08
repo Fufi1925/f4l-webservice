207eaa6fe43de156ab4e80581bf15f74f7b40313
Fufi1925 <laurenzspicker@gmail.com>
2026-02-08 11:32:37 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
+fetch("/api/stats")
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("online").textContent = data.online;
+  });
 
+fetch("/api/me")
+  .then(res => res.json())
+  .then(user => {
+    if (!user) return;
+
+    document.getElementById("userBox").innerHTML = `
+      <p>👤 ${user.username}</p>
+      ${user.isAdmin ? "<b>ADMIN PANEL AKTIV</b>" : ""}
+      <br><a href="/logout">Logout</a>
+    `;
+  });
